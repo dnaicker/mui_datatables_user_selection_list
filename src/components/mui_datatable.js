2e673a5289aaf661de1c0d12bb6ac2264627d24d
Denver <dnaicker@gmail.com>
2019-07-10 14:03:36 +0200

    create react app, updated readme, first page created, clean up as well

---
+++ src/components/mui_datatable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MUIDataTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>MUIDataTable</div>
+    );
+  }
+}
+
+export default MUIDataTable;
